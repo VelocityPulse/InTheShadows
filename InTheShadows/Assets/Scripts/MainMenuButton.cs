1bf1ea1af8a87eb42ad7b7cce5922b32d1036a98
Clement CHAMEYRAT <cchameyr@e1r2p4.42.fr>
2018-06-26 16:05:49 +0200

    auto save

---
+++ InTheShadows/Assets/Scripts/MainMenuButton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MainMenuButton : MonoBehaviour, UnityEngine.EventSystems.IPointerDownHandler {
+
+	public bool debugMode = false;
+	private UnityEngine.UI.Selectable selectable;
+
+	// Use this for initialization
+	void Start () {
+		selectable = GetComponent<UnityEngine.UI.Selectable> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//selectable.OnPointerEnter ()
+
+
+	}
+
+	public void OnPointerEnter (PointerEventData eventData) {
+		Debug.Log ("The cursor entered the selectable UI element.");
+	}
+
+	private void OnMouseDown () {
+		Debug.Log ("down");
+	}
+
+	public void OnPointerDown (PointerEventData eventData) {
+		Debug.Log ("Click");
+	}
+}

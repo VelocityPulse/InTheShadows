9d2e8510a813c12d72e145520a85ee839221eb18
Clement CHAMEYRAT <cchameyr@e1r2p4.42.fr>
2018-06-26 16:44:31 +0200

    auto save

---
+++ InTheShadows/Assets/Scripts/MainMenuCamera.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuCamera : MonoBehaviour {
+
+	enum MenuScene {
+		HOME,
+		SELECTOR
+	}
+
+	private MenuScene scene = MenuScene.HOME;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void goToSelectorScene () {
+
+	}
+
+	public void backToHomeScene () {
+
+	}
+
+}

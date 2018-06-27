87a59ef3f87dc5f2f4a09af9c7be3c971e42a78c
Clement CHAMEYRAT <cchameyr@e3r10p19.42.fr>
2018-06-27 13:06:55 +0200

    auto save

--- InTheShadows/Assets/Scripts/GameManager.cs
+++ InTheShadows/Assets/Scripts/GameManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
+	List<GameObject> selectorLevelButtons = new List<GameObject>();
+
 	// Use this for initialization
 	void Start () {
 		
---
+++ InTheShadows/Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+
+	[HideInInspector] public int levelStatus1 = false;
+	[HideInInspector] public int levelStatus2 = false;
+	[HideInInspector] public int levelStatus3 = false;
+	[HideInInspector] public int levelStatus4 = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
